6af1dc994ce974badce38f202acd4f4dcfa9e6ac
Carson Farmer <carson.farmer@gmail.com>
2018-06-12 09:18:32 -0700

    Initial public commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Texile's IPFS-based xkcd ĐApp
+
+> Simple ĐApp for protecting the web's most [precious resource](https://xkcd.com/)
+
+## Install
+
+1. This app works best with `window.ipfs`. Install the IPFS Companion web extension:
+
+    <a href="https://addons.mozilla.org/en-US/firefox/addon/ipfs-companion/" title="Get the add-on"><img width="86" src="https://blog.mozilla.org/addons/files/2015/11/AMO-button_1.png" /></a> <a href="https://chrome.google.com/webstore/detail/ipfs-companion/nibjojkomfdiaoajekhjakgkdhaomnch" title="Get the extension"><img width="103" src="https://developer.chrome.com/webstore/images/ChromeWebStore_BadgeWBorder_v2_206x58.png" /></a>
+
+2. Install dependencies `yarn install`
+3. Build the app `yarn build`
+4. Start the app `yarn start`
+
+### Develop
+
+Instead of steps 3 & 4: `yarn watch`
+
+### Thanks
+
+Built using [Textile's IPFS ĐApp Template](https://github.com/textileio/dapp-template).
---
+++ src/main.js
@@ -0,0 +1,99 @@
+import 'babel-polyfill'
+import getIpfs from 'window.ipfs-fallback'
+
+var ipfs
+const xkcdRoot = '/ipfs/QmWEAXcqwq5zY2u8Z1mii5m3MXricctd7efFep7sSEWZQz'
+
+const fetchAndReplaceComic = async comicNumber => {
+  try {
+    const files = await ipfs.files.get(`${xkcdRoot}/${comicNumber}`)
+    for (let file of files) {
+      const content = String.fromCharCode.apply(null, file.content)
+      const image = document.getElementById('image')
+      if (file.name.endsWith('.png')) {
+        // Extract image data as base64 encoded string
+        const encoded = btoa(content)
+        // Update comic image
+        image.setAttribute('src', `data:image/png;base64,${encoded}`)
+        // Update original permalink
+        document.getElementById('permalink')
+          .textContent = comicNumber
+        // Update IPFS hotlink
+        document.getElementById('hotlink')
+          .textContent = `${xkcdRoot}/${file.path}`
+      } else if (file.name.endsWith('alt.txt')) {
+        // Update comic title (hover)
+        image.setAttribute('title', content)
+        // Update comic title
+        const title = file.name.slice(file.name.indexOf('- ') + 2, -10)
+        document.getElementById('ctitle').textContent = title
+        // Update comic image alt text
+        image.setAttribute('alt', title)
+      } else if (file.name.endsWith('alt.txt')) {
+        document.getElementById('transcript')
+          .textContent = content
+      }
+    }
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const setup = async () => {
+  try {
+    // Create new ipfs peer instance (or use browser-based peer)
+    ipfs = await getIpfs()
+    // Connect to public peer pinning xkcd comics (normally not needed)
+    await ipfs.swarm.connect(
+      '/ip4/54.201.10.69/tcp/4002/ws/ipfs/QmQqBfDcAre7CaTMmMzHqnrHPxX3xVT5D5GaiHh4zppTL7'
+    )
+    // Add button event listeners
+    for (let element of document.getElementsByClassName('first')) {
+      element.addEventListener('click', (event) => {
+        event.preventDefault()
+        fetchAndReplaceComic(1)
+      }, true)
+    }
+    for (let element of document.getElementsByClassName('last')) {
+      element.addEventListener('click', (event) => {
+        event.preventDefault()
+        fetchAndReplaceComic(2003)
+      }, true)
+    }
+    for (let element of document.getElementsByClassName('random')) {
+      element.addEventListener('click', (event) => {
+        event.preventDefault()
+        fetchAndReplaceComic(Math.floor(Math.random() * 2003) + 1)
+      }, true)
+    }
+    for (let element of document.getElementsByClassName('prev')) {
+      element.addEventListener('click', (event) => {
+        event.preventDefault()
+        const current = parseInt(
+          document.getElementById('permalink').textContent
+        )
+        if (current > 1) {
+          fetchAndReplaceComic(current - 1)
+        }
+      }, true)
+    }
+    for (let element of document.getElementsByClassName('next')) {
+      element.addEventListener('click', (event) => {
+        event.preventDefault()
+        const current = parseInt(
+          document.getElementById('permalink').textContent
+        )
+        if (current < 2002) {
+          fetchAndReplaceComic(current + 1)
+        }
+      }, true)
+    }
+  } catch(err) {
+    console.log(err)
+  }
+  // Choose random comic (we have the first 2003 available right now)
+  await fetchAndReplaceComic(Math.floor(Math.random() * 2003) + 1)
+  document.getElementById('loading').style.visibility = "hidden"
+  document.getElementById('middleContainer').style.visibility = "visible"
+}
+setup()
